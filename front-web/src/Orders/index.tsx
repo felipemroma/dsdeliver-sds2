ea58aafa0a1d6bb75cf46d395f5e81174669fa7e
felipemroma <felipe.roma@yahoo.com.br>
2021-01-09 08:44:23 -0300

    Add Routers and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import Footer from '../Footer'
+import StepsHeader from './StepsHeader'
+import './styles.css'
+
+
+function Orders(){
+    return (
+
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders;

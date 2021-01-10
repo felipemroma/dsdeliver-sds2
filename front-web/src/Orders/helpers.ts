dce7830ef30bbf6c9023033a73a7dcbba9dce563
felipemroma <felipe.roma@yahoo.com.br>
2021-01-10 16:23:01 -0300

    Finish front-web

--- front-web/src/Orders/helpers.ts
+++ front-web/src/Orders/helpers.ts
@@ -8,7 +8,7 @@ export function formatPrice(price: number) {
   const formatter = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
-  })
+  }) 
 
   return formatter.format(price)
 }
--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -6,9 +6,16 @@ export type Product = {
     imageUri: string
   }
 
-
   export type OrderLocationData = {
     latitude: number,
     longitude: number,
     address: string
   }
+
+  type ProductId = {
+    id: number
+  }
+  
+  export type OrderPayLoad = {
+    products: ProductId[],
+  } & OrderLocationData
